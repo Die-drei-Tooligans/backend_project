a5e877af543cb40b5610da9ac9167be66f8e3542
laraannika <matzoldlara@gmail.com>
2025-01-09 11:20:38 +0100

    userModel finished!?

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,31 +1,67 @@
 import mongoose from "mongoose";
 
-const adressSchema = new mongoose.Schema({
-	city: { type: String },
-	zipCode: { type: Number },
-	street: { type: String },
-	streetNum: { type: Number },
-	numSupp: { type: String },
+const userSchema = new mongoose.Schema({
+	firstname: {type:String, required:true},
+	lastname: {type:String, required:true},
+	image: {type:String},
+	email: {type:String, required:true, unique:true},
+	password: {type:String, required:true},
+	phone: {type:Number, required:true, unique:true},
+	address: {type:addressSchema, required:true},
+	dob: {type:Date, required:true},
+	isAdmin: {type:Boolean, required:true},
+	cars: {
+		type: [
+			{
+				type: carSchema
+			}
+		],
+		default:[]
+	},
+	bookings: {
+		type: [
+			{
+				type: bookingSchema
+			}
+		],
+		default:[]
+	},
+	company: {type:companySchema}
+}, {minimize:false}, {timestamps:true})
+
+//! IF isAdmin === true -> company is required, cars is NOT
+//! IF isAdmin === false -> company is NOT required, cars is required
+
+const addressSchema = new mongoose.Schema({
+
+	city: {type:String, required:true},
+	zipCode: {type:Number, required:true},
+	street: {type:String, required:true},
+	streetNum: {type:Number, required:true},
+	numSupp: {type:String, required:true}
 });
 
-const userSchema = new mongoose.Schema(
-	{
-		firstname: { type: String },
-		username: { type: String, unique: true },
-		mail: { type: String },
-		password: { type: String },
-		adress: { type: adressSchema },
-	},
-	{
-		timestamps: true,
-	}
-);
+export const userModel = mongoose.models.user || mongoose.model('user', userSchema)
 
+// carSchema (erledigte Reperaturen, Dokumente), bookingSchema, companySchema
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 userSchema.methods.toJSON = function () {
 	const obj = this.toObject();
 	delete obj.password;
 	delete obj._id;
 	return obj;
 };
+*/
 
-export const User = mongoose.model("Users", userSchema);
