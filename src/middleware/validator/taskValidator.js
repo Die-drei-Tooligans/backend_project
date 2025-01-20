acc65192293840b1b7b72dd533b53d5cd5fe7182
Ju Chang <code4ju@gmail.com>
2025-01-20 15:23:48 +0100

    validation zwischenspeichern

--- src/middleware/validator/taskValidator.js
+++ src/middleware/validator/taskValidator.js
@@ -4,24 +4,31 @@ export const taskValidator = [
 	body("deleted").isBoolean().default(false),
 	body("fitShop").notEmpty().withMessage("Must be a String"),
 	body("name")
+		.optional()
 		.isString()
 		.isLength({ min: 5, max: 40 })
 		.withMessage("Invalid input"),
 	body("topic")
+		.optional()
 		.isString()
 		.isLength({ min: 5, max: 40 })
 		.withMessage("String has to be 5 to 40 characters long"),
-	body("image").isURL().withMessage("Has to be valid URL"),
+	body("image").optional().isURL().withMessage("Has to be valid URL"),
 	body("about")
+		.optional()
 		.isString()
 		.isLength({ min: 5, max: 100 })
 		.withMessage("String has to be 5 to 100 characters long"),
 
-	body("timeRequired").isNumeric().withMessage("Has to be a number."),
-	body("isFree").isBoolean().withMessage("Must be boolean"),
-	body("isDoneBefore").isBoolean().withMessage("Must be boolean"),
-	body("doneLast").isDate().withMessage("Must be date."),
-	body("costs").isNumeric().withMessage("Must be a number"),
+	body("timeRequired")
+		.optional()
+		.isNumeric()
+		.default(15)
+		.withMessage("Has to be a number."),
+	body("isFree").optional().isBoolean().withMessage("Must be boolean"),
+	body("isDoneBefore").optional().isBoolean().withMessage("Must be boolean"),
+	body("doneLast").optional().isDate().withMessage("Must be date."),
+	body("costs").optional().isNumeric().withMessage("Must be a number"),
 	body("neededMaterials").optional().isArray().withMessage("Must be Array"),
 	body("neededMaterials.name")
 		.optional()
